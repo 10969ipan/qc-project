36baa26d6e1aa8f2e6f37ae1c342b57c72a4acff
10969ipan <arfianirfan48@gmail.com>
2025-12-19 02:13:01 +0700

    new user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,6 +42,13 @@ class DatabaseSeeder extends Seeder
             'role' => 'inspector',
         ]);
 
+        User::create([
+            'name' => 'Anggi Purnama',
+            'email' => 'anggi@qc.com',
+            'password' => Hash::make('password'),
+            'role' => 'inspector',
+        ]);
+
         // Buat Akun Ka. Shift
         User::create([
             'name' => 'Ka Shift User',
