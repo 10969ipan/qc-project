4fd4118b832086cd20f90609541283b4b76a3e66
10969ipan <arfianirfan48@gmail.com>
2025-12-26 10:33:37 +0700

    tambah barang

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('role:admin,supervisor,kashift,asst_manager,manager')->except('index');
-    }
-
     public function index(Request $request)
     {
         $query = Item::query();
