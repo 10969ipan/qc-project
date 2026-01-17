d91443a68073dcafa5f3ebd77efaa33009f6d95c
10969ipan <arfianirfan48@gmail.com>
2026-01-17 18:09:24 +0700

    Fix: Load ApprovalHelper in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Load helper functions
+require_once __DIR__ . '/../app/Helpers/ApprovalHelper.php';
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
