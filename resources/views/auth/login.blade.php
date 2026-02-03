f9f4d39c345ba1fecc28baeba5ca144ef4ffdca8
10969ipan <arfianirfan48@gmail.com>
2026-02-03 10:56:18 +0700

    fix: update logo and favicon for server compatibility

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="">
 
     <title>MASUK - QC APPS</title>
+    <link rel="icon" href="{{ asset('master item/ipp.png') }}" type="image/png">
 
     <link href="{{ asset('startbootstrap-sb-admin-2-gh-pages/vendor/fontawesome-free/css/all.min.css') }}"
         rel="stylesheet" type="text/css">
@@ -448,7 +449,7 @@
                 </form>
             </div>
             <div class="login-side-image">
-                <img src="{{ asset('master item/PNG LOGO IPP.png') }}"
+                <img src="{{ asset('master item/PNG LOGO IPP.png') }}" alt="Logo IPP"
                     style="position: absolute; top: 20px; right: 20px; width: 100px; z-index: 30;">
                 <div class="banner-content">
                     <h2 class="banner-title">QC APPS<br></h2>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard')</title>
-    <link rel="icon" href="{{ asset('master item/PNG LOGO IPP.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('master item/ipp.png') }}" type="image/png">
 
     <link href="{{ asset('startbootstrap-sb-admin-2-gh-pages/vendor/fontawesome-free/css/all.min.css') }}"
         rel="stylesheet" type="text/css">
