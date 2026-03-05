e40ac422d65c13ded168eb213ab5be85039c57f3
10969ipan <arfianirfan48@gmail.com>
2026-03-05 16:11:20 +0700

    feat: make part weight optional and update validation rules

--- app/Models/InProcessChecksheet.php
+++ app/Models/InProcessChecksheet.php
@@ -45,6 +45,7 @@ class InProcessChecksheet extends Model
     protected $casts = [
         'defects' => 'array',
         'dimension_check' => 'array',
+        'part_weight' => 'array',
         'date' => 'date',
     ];
 
